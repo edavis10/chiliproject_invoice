5db18554ac0b3dc35bacff28d29c7ae300cd986b
Eric Davis <edavis@littlestreamsoftware.com>
2008-03-13 05:32:55 +0000

    Converting description into HTML with RedCloth, #625

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,9 @@
 class Invoice < ActiveRecord::Base
   belongs_to :customer
+  
+  before_save :textilize
+  
+  def textilize
+    self.description_html = RedCloth.new(self.description).to_html
+  end
 end
