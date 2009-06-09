24d52b78cf35439832abae90b3b483ac3ef8136b
Eric Davis <edavis@littlestreamsoftware.com>
2009-06-08 21:18:16 -0700

    Renamed RedmineCompatibility to InvoiceCompatibility

--- lib/invoice_compatibility.rb
+++ lib/invoice_compatibility.rb
@@ -1,10 +1,10 @@
 # Wrappers around the Redmine core API changes between versions
-module RedmineCompatibility
+module InvoiceCompatibility
   class Enumeration
     # Wrapper around Redmine's API since Enumerations changed in r2472
     # This can be removed once 0.9.0 is stable
     def self.activities
-      if Enumeration.respond_to?(:activities)
+      if ::Enumeration.respond_to?(:activities)
         return ::Enumeration.activities
       else
         return ::Enumeration::get_values('ACTI')
